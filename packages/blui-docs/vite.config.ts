ed834914ab797ee493828db77c0cd0b8fc2f9afd
bretski00 <bretski00@users.noreply.github.com>
2025-07-20 17:10:07 -0500

    Fixing the building locally

--- packages/blui-docs/vite.config.ts
+++ packages/blui-docs/vite.config.ts
@@ -1,9 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      // In development, import directly from source files for instant feedback
+      'blui': path.resolve(__dirname, '../blui-core/src/index.ts')
+    }
+  },
   server: {
     port: 5173
   }
